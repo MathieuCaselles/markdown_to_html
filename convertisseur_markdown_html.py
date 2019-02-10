42d8d79a84268e8f34bb2a2adbc789a93748c163
Dieu <mathieu.caselles@ynov.com>
2019-02-10 20:03:02 +0100

    Projet terminé
    Ajout de la fonction kikoo et petit coup de black pour la pep 8

--- convertisseur_markdown_html.py
+++ convertisseur_markdown_html.py
@@ -1,8 +1,10 @@
 import os
 import argparse
 import markdown2
+from random import randint
+from random import choice
 
-# interface graphique
+# outil en ligne de commande
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -12,23 +14,28 @@ parser.add_argument(
     type=str,
 )
 parser.add_argument(
-    "-o", 
-    "--output-directory", 
-    help="Le dossier de destination", 
-    type=str,
+    "-o", "--output-directory", help="Le dossier de destination", type=str
 )
 parser.add_argument(
     "-a",
     "--achtung",
-    help = "Activer l'option permet de rendre le texte plus compréhensible aux allemands",
-    action = "store_true"
+    help="Activer l'option permet de rendre le texte plus compréhensible aux allemands",
+    action="store_true",
+)
+parser.add_argument(
+    "-k",
+    "--kikoo_lol",
+    help="Activer l'option permet d'ajouter dans le texte des « kikoo », « lol », « mdr », « ptdr » de manière aléatoire",
+    action="store_true",
 )
 
 
 arg = parser.parse_args()
 
 if arg.input_directory == None or arg.output_directory == None:
-    print("IL vous manque quelque chose: Utilisez «python convertisseur_markdown_html.py -h» ou sinon vous pouvez aussi vous référer au readme")
+    print(
+        "IL vous manque quelque chose: Utilisez «python convertisseur_markdown_html.py -h» ou sinon vous pouvez aussi vous référer au readme"
+    )
 
 else:
     dossier_1 = os.listdir(arg.input_directory)
@@ -37,63 +44,88 @@ else:
 
 # Structure de la page html en 2 parties.
 
-debut_html = "<!DOCTYPE html>\n\n<html>\n\n<head>\n\n\t<meta charset='utf-8'/>\n\t<title>"+ "Fichier convertit" +"</title>\n\n</head>\n\n<body>\n\n"
+debut_html = (
+    "<!DOCTYPE html>\n\n<html>\n\n<head>\n\n\t<meta charset='utf-8'/>\n\t<title>"
+    + "Fichier convertit"
+    + "</title>\n\n</head>\n\n<body>\n\n"
+)
 fin_html = "\n\n</body>\n\n</html>"
 
 # achtung
 
+
 def achtung(texte):
 
-    texte = texte.replace('s', 'z')
-    texte = texte.replace('co', 'ko')
-    texte = texte.replace('ca', 'ka')
-    texte = texte.replace('cu', 'ku')
-    texte = texte.replace('cc', 'k')
-    texte = texte.replace('c ', 'k ')
-    texte = texte.replace('c', 'z')
-    texte = texte.replace('qu', 'k')
-    texte = texte.replace('ph', 'f')
-    texte = texte.replace('zz', 'z')
-    texte = texte.replace('mm', 'm')
-    texte = texte.replace('nn', 'n')
-    texte = texte.replace('tt', 't')
-    texte = texte.replace('rr', 'r')
-    texte = texte.replace('co', 'ko')
-    texte = texte.replace('ff', 'f')
-    texte = texte.replace('ll', 'l')
-    texte = texte.replace('pp', 'p')
-    texte = texte.replace('bb', 'b')
-    texte = texte.replace('dd', 'd')
-    texte = texte.replace('gg', 'g')
-    texte = texte.replace('kk', 'k')
-    texte = texte.replace('ge', 'che')
-    texte = texte.replace('gi', 'chi')
-    texte = texte.replace('co', 'ko')
-    texte = texte.replace('g', 'k')
-    texte = texte.replace('b', 'p')
-    texte = texte.replace('v', 'f')
-    texte = texte.replace('zh', 'ch')
-    texte = texte.replace('e ', ' ')
+    texte = texte.replace("s", "z")
+    texte = texte.replace("co", "ko")
+    texte = texte.replace("ca", "ka")
+    texte = texte.replace("cu", "ku")
+    texte = texte.replace("cc", "k")
+    texte = texte.replace("c ", "k ")
+    texte = texte.replace("c", "z")
+    texte = texte.replace("qu", "k")
+    texte = texte.replace("ph", "f")
+    texte = texte.replace("zz", "z")
+    texte = texte.replace("mm", "m")
+    texte = texte.replace("nn", "n")
+    texte = texte.replace("tt", "t")
+    texte = texte.replace("rr", "r")
+    texte = texte.replace("co", "ko")
+    texte = texte.replace("ff", "f")
+    texte = texte.replace("ll", "l")
+    texte = texte.replace("pp", "p")
+    texte = texte.replace("bb", "b")
+    texte = texte.replace("dd", "d")
+    texte = texte.replace("gg", "g")
+    texte = texte.replace("kk", "k")
+    texte = texte.replace("ge", "che")
+    texte = texte.replace("gi", "chi")
+    texte = texte.replace("co", "ko")
+    texte = texte.replace("g", "k")
+    texte = texte.replace("b", "p")
+    texte = texte.replace("v", "f")
+    texte = texte.replace("zh", "ch")
+    texte = texte.replace("e ", " ")
+    texte = texte.replace("htpz", "https")
 
     return texte
+
+
+# kikoo
+
+
+def kikoo(texte):
+
+    for i in range(int(len(texte) / 50)):
+
+        ajout = randint(0, len(texte))
+        mot = choice(["kikoo", "lol", "mdr", "ptdr"])
+        texte = texte[:ajout] + mot + texte[ajout:]
+
+    return texte
+
 
 # Gestion des fichier dans dossier
 
 for fichier_a_convertir in dossier_1:
 
-    if fichier_a_convertir.endswith('.md'):
+    if fichier_a_convertir.endswith(".md"):
 
         with open("conversion/" + fichier_a_convertir, "r") as fichier:
             mon_fichier = fichier.read()
-        
-        nouveau_nom = fichier_a_convertir.replace('.md', '')
+
+        nouveau_nom = fichier_a_convertir.replace(".md", "")
+
         if arg.achtung:
             mon_fichier = achtung(mon_fichier)
+        if arg.kikoo_lol:
+            mon_fichier = kikoo(mon_fichier)
 
         with open(dossier_2 + "/" + nouveau_nom + ".html", "x") as fichier:
             fichier.write(debut_html)
-            fichier.write(markdown2.markdown(mon_fichier)) # markdown2.markdown() fait la conversion automatiquement ! C'est super tchité !
+            fichier.write(
+                markdown2.markdown(mon_fichier)
+            )  # markdown2.markdown() fait la conversion automatiquement ! C'est super tchité !
             fichier.write(fin_html)
 
 print("Conversion réussis !")
-
