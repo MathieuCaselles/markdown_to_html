733184f9eede0739dc0e5cce8a96870e78101316
Dieu <mathieu.caselles@ynov.com>
2019-02-10 15:42:46 +0100

    Suppression des fichier python de test + Conversion v1 fonctionnelle
    La conversion fonctionne mais pour 1 fichier a la fois actuellement. Le prochain push permettra de convertir tous les fichier d'un dossier !

--- conversion/mon_test.md
+++ conversion/mon_test.md
@@ -1,2 +1,16 @@
-# Ceci est juste un test
-## J'espère qae cela va se convertir
+# Ceci est juste un fichier test test
+## J'espère que cela va se convertir
+### même avec un h3
+#### et pourquoi pas un h4
+##### Soyons fou un h5 !
+## On revient sur un h2 ?
+###### On peut faire des titre a l'infini ! nous voila en h6 !!!
+
+* ceci est censé être une liste non ordonné qui a été écrit avec une astérix.
+* idem pour celle la
+* jamais 2 sans 3 !
+
+Ceci est un texte _dont_ certains __éléments__ sont formatés **pour _être_** en *italique* ou en **bold**.  
+
+D'ailleur cette phrase a été trouvé sur ce lien ! <https://blog.wax-o.com/2014/04/tutoriel-un-guide-pour-bien-commencer-avec-markdown/>
+
--- convertisseur_markdown_html.py
+++ convertisseur_markdown_html.py
@@ -21,25 +21,26 @@ parser.add_argument(
 arg = parser.parse_args()
 
 if arg.input_directory == None or arg.output_directory == None:
-    print("IL vous manque quelque chose: Utilisez --help ou sinon vous pouvez aussi vous référer au readme")
+    print("IL vous manque quelque chose: Utilisez «python convertisseur_markdown_html.py -h» ou sinon vous pouvez aussi vous référer au readme")
 
 else:
     dossier_1 = arg.input_directory
 
     dossier_2 = arg.output_directory
 
-# Gestion des fichier dans dossier
+# Structure de la page html en 2 parties.
 
 debut_html = "<!DOCTYPE html>\n\n<html>\n\n<head>\n\n\t<meta charset='utf-8'/>\n\t<title>"+ "Fichier convertit" +"</title>\n\n</head>\n\n<body>\n\n"
 fin_html = "\n\n</body>\n\n</html>"
 
+# Gestion des fichier dans dossier
+
 with open(dossier_1, "r") as fichier:
     mon_fichier = fichier.read()
 
-
 with open(dossier_2, "x") as fichier:
     fichier.write(debut_html)
-    fichier.write(mon_fichier)
+    fichier.write(markdown2.markdown(mon_fichier)) # markdown2.markdown() fait la conversion ausomatiquement ! C'est super tchité !
     fichier.write(fin_html)
 
 with open(dossier_2, "r") as fichier:
@@ -47,6 +48,6 @@ with open(dossier_2, "r") as fichier:
 
 print("Conversion réussis !")
 
-# convertion
+# conversion
 
 
